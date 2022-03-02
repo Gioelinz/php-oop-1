5eb3e7960425239b84fd6176af18550ed277ab01
Gioelinz <gioelerunza.gr@gmail.com>
2022-03-02 17:04:46 +0100

    add echo with array

--- index.php
+++ index.php
@@ -14,31 +14,51 @@ class Movie
     {
         $this->id = $id;
         $this->title = $title;
-        $this->genre = $genre;
+        $this->genre = ucwords($genre);
         $this->description = $description;
     }
 
     public function getSubString()
     {
-        return substr($this->description, 0, 50) . '...';
+        return substr($this->description, 0, 100) . '...';
     }
 }
 
-$text1 = 'Dopo essere sopravvissuto ad un attacco inaspettato in territorio nemico, l\'industriale Tony Stark costruisce un\'armatura ad alta tecnologia e giura di proteggere il mondo nei panni di Iron Man.';
+/* $text1 = 'Dopo essere sopravvissuto ad un attacco inaspettato in territorio nemico, l\'industriale Tony Stark costruisce un\'armatura ad alta tecnologia e giura di proteggere il mondo nei panni di Iron Man.';
 $text2 = 'L\'identità dell\'Uomo Ragno viene rivelata a tutti, e non riesce più a separare la sua vita normale dalla vita da supereroe, e quando chiede aiuto al Dottor Strange, lo costringe a scoprire cosa significa veramente essere l\'Uomo Ragno.';
 $text3 = 'Un gruppo di adolescenti scopre l\'esistenza di un\'entità malvagia che si nutre delle paure degli esseri umani e che ha le sembianze di un pagliaccio di nome Pennywise. L\'essere, che dimora nelle profondità della rete fognaria, è solo uno dei volti della creatura millenaria nota come It, un mostro senza forma che si risveglia ciclicamente per mietere vittime tra i bambini della città. Per sconfiggerlo, i ragazzi devono restare uniti e mantenere vivo il sentimento di amicizia che li lega.';
 
 $film1 = new Movie(1, 'Iron Man', 'action', $text1);
 $film2 = new Movie(1, 'Spider-man No Way Home', 'action', $text2);
-$film3 = new Movie(1, 'IT', 'horror', $text3);
+$film3 = new Movie(1, 'IT', 'horror', $text3); */
 
 
 //echo $film1->description;
 
-echo $film3->getSubString();
-echo '<br>' . $film2->getSubString();
+/* echo $film3->getSubString();
+echo '<br>' . $film2->getSubString(); */
 
+?>
 
-var_dump($film1);
-var_dump($film2);
-var_dump($film3);
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movies Obj</title>
+</head>
+
+<body>
+
+    <?php foreach ($movies as $movie) {
+        $obj_movie = new Movie($movie['id'], $movie['title'], $movie['genre'], $movie['description']); ?>
+        <h1><?= $obj_movie->title ?></h1>
+        <address><?= $obj_movie->genre ?></address>
+        <p><?= $obj_movie->getSubString() ?></p>
+    <?php } ?>
+
+</body>
+
+</html>
