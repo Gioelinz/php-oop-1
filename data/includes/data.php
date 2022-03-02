27823ce7c5c85f82e5aee2b72fe559043b2cc1cc
Gioelinz <gioelerunza.gr@gmail.com>
2022-03-02 15:53:05 +0100

    add data array

---
+++ data/includes/data.php
@@ -0,0 +1,41 @@
+<?php
+
+$movies = [
+    [
+        'id' => '1',
+        'title' => 'Iron Man',
+        'genre' => 'action',
+        'description' => 'Dopo essere sopravvissuto ad un attacco inaspettato in territorio 
+        nemico, l\'industriale Tony Stark costruisce un\'armatura ad alta tecnologia e 
+        giura di proteggere il mondo nei panni di Iron Man.'
+    ],
+    [
+        'id' => '2',
+        'title' => 'Spider-man No Way Home',
+        'genre' => 'action',
+        'description' => 'L\'identità dell\'Uomo Ragno viene rivelata a tutti, e non 
+        riesce più a separare la sua vita normale dalla vita da supereroe, e quando chiede 
+        aiuto al Dottor Strange, 
+        lo costringe a scoprire cosa significa veramente essere l\'Uomo Ragno.'
+    ],
+    [
+        'id' => '3',
+        'title' => 'IT',
+        'genre' => 'horror',
+        'description' => 'Un gruppo di adolescenti scopre l\'esistenza di un\'entità malvagia 
+        che si nutre delle paure degli esseri umani e che ha le sembianze di un pagliaccio di
+         nome Pennywise. L\'essere, che dimora nelle profondità della rete fognaria, è solo uno 
+         dei volti della creatura millenaria nota come It, un mostro senza forma che si risveglia 
+         ciclicamente 
+         per mietere vittime tra i bambini della città. Per sconfiggerlo, 
+        i ragazzi devono restare uniti e mantenere vivo il sentimento di amicizia che li lega.'
+    ],
+    [
+        'id' => '4',
+        'title' => 'Eternals',
+        'genre' => 'fantasy',
+        'description' => 'Dopo gli eventi accaduti in Avengers: Endgame, una tragedia 
+        inaspettata obbliga gli Eterni a uscire dall\'ombra e riunire le forze contro il più 
+        antico dei nemici dell\'umanità, i Devianti.'
+    ]
+];
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+include __DIR__ . '/data/includes/data.php';
+
 class Movie
 {
     public $id;
